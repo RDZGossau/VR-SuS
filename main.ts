2f458890ed4e088f0a32b61d8aeac20444f4cfac
RDZ Gossau <94575224+RDZGossau@users.noreply.github.com>
2022-01-11 14:15:28 +0100

    Pager für SuS zur Aufforderung an den VR-Posten oder zur Pause

--- main.ts
+++ main.ts
@@ -9,10 +9,19 @@ radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(MicroBitNr)
 })
 input.onButtonPressed(Button.A, function () {
+    radio.sendString("HALLO BUDDY")
     basic.showIcon(IconNames.Happy)
-    music.playMelody("E G B C5 - - - - ", 190)
-    basic.pause(4500)
+    music.playMelody("- A A F A - - - ", 200)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(5000)
 })
+input.onGesture(Gesture.SixG, function () {
+    for (let index = 0; index < 1; index++) {
+        basic.showIcon(IconNames.Ghost)
+        music.playMelody("C5 A B G A F G E ", 326)
+    }
+})
+// Damit kann der Damit kann der Funkkanal um 1 erhöht und damit geändert werden.
 input.onButtonPressed(Button.AB, function () {
     if (MicroBitNr >= 12) {
         MicroBitNr = 1
@@ -30,7 +39,7 @@ input.onButtonPressed(Button.AB, function () {
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "PAUSE") {
         for (let index = 0; index < 3; index++) {
-            soundExpression.yawn.playUntilDone()
+            music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
             basic.showLeds(`
                 . # . # .
                 . # . # .
@@ -40,25 +49,38 @@ radio.onReceivedString(function (receivedString) {
                 `)
             basic.pause(2000)
             basic.showString(receivedString)
-            basic.clearScreen()
+        }
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            `)
+        basic.pause(5000)
+    } else {
+        for (let index = 0; index < 2; index++) {
+            basic.showIcon(IconNames.Happy)
+            music.playMelody("- - C5 A C5 - - - ", 200)
+            basic.showString(receivedString)
+            basic.showIcon(IconNames.Happy)
+            basic.pause(2000)
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(MicroBitNr)
-    basic.pause(3000)
-})
-input.onGesture(Gesture.ThreeG, function () {
-    for (let index = 0; index < 2; index++) {
-        basic.showIcon(IconNames.Ghost)
-        music.playMelody("C5 A B G A F G E ", 326)
-    }
+    music.playMelody("F - F - - - - - ", 200)
+    basic.showNumber(MicroBitNr)
+    basic.pause(2000)
 })
 let MicroBitNr = 0
-MicroBitNr = 1
+// Bitte diesen WERT vor dem Download auf das jeweilge M:B-Paar anpassen!!
+MicroBitNr = 12
 radio.setGroup(MicroBitNr)
 basic.showNumber(MicroBitNr)
 basic.pause(3000)
+// Leuchtdioden zeigen an, dass der MB eingeschaltet ist.
 basic.forever(function () {
     basic.showLeds(`
         # . . . #
